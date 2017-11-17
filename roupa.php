e33ff322dcbac1ed7ce7be485979f6c645be28b1
Marcelo Bonatto <marcelo_bonatto@hotmail.com>
2017-11-17 17:10:41 -0200

    Criação da gravação da tela de cadastro de roupa

--- roupa.php
+++ roupa.php
@@ -13,7 +13,9 @@ include('header.php');
     <div class="conteudo">
         <h1>Cadastro de Roupa - <?php echo(($id != 'novo' ? '<< nome da roupa >>' : 'Novo Cadastro')); ?></h1>
         <br />
-        <form>
+        <div id="mensagem" class="alert alert-danger d-none" role="alert">
+        </div>
+        <form id="frmRoupa">
             <div class="form-group">
                 <label for="txtId">Código Interno:</label>
                 <input class="form-control col-sm-4" type="text" value="" id="txtId" name="txtId" readonly="readonly" />
@@ -40,8 +42,32 @@ include('header.php');
                 <label for="txtBonus">Bônus (%):</label>
                 <input class="form-control col-sm-2" type="number" value="" id="txtBonus" name="txtBonus" min="0" max="100" required />
             </div>
+            <div class="form-group">
+                <label for="txtPreco">Preço:</label>
+                <input class="form-control col-sm-3" type="number" value="" id="txtBonus" name="txtPreco" min="0" max="10000" step="10" required />
+            </div>
+            <div class="form-group">
+                <label>Ativo:</label>
+                <br />
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-success active">
+                        <input type="radio" name="optAtivo" id="optSim" autocomplete="off" value="1" checked> Sim
+                    </label>
+                    <label class="btn btn-secondary">
+                        <input type="radio" name="optAtivo" id="optNao" autocomplete="off" value="0"> Não
+                    </label>
+                </div>
+            </div>
+            <hr />
+            <button class="btn btn-info">
+                Gravar
+            </button>
+            <button class="btn btn-danger">
+                Voltar
+            </button>
         </form>
     </div>
 <?php
+$js[]   = 'js/roupa.js';
 include('footer.php');
 ?>
