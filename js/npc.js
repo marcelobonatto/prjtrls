c6012303467c6745c59e23638e409c860abc9010
marlonvolz <marlonvolz@gmail.com>
2017-11-18 21:22:18 -0200

    CRUD NPC
    crud npc

---
+++ js/npc.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    var form = $("#frmNpc");
+
+    form.submit(function(event) {
+        var ehValido = form[0].checkValidity();
+
+        // cancels the form submission
+        event.preventDefault();
+
+        if (ehValido)
+        {
+            var mensagem = $("#mensagem");
+
+            if (!mensagem.hasClass("d-none"))
+            {
+                mensagem.addClass("d-none");
+            }
+
+            submitForm();
+        }
+    });
+
+    function submitForm() {
+        // Initiate Variables With Form Content
+        var id = $("#txtId").val();
+        var nome = $("#txtNome").val();
+        var chave = $("#txtChave").val();
+        var eixo = $("#cmbEixo").find("option:selected").val();
+        var imagemNormal = $("#txtImgNormal").val();
+        var icone = $("#txtIcone").val();
+        var ativo = $("input[name='optAtivo']:checked").val();
+
+        $.ajax({
+            type: "POST",
+            url: "exec/gravarnpc.php",
+            data: "id=" + id + "&nome=" + nome + "&chave=" + chave + "&eixo=" + eixo + "&imagemNormal=" + imagemNormal + "&icone=" + icone + "&ativo=" + ativo,
+            success : function(text) {
+                var txtspl = text.split("|");
+
+                if (txtspl[0] == "OK") {
+                    var mensagem = $("#mensagem");
+                    
+                    if (mensagem.hasClass("alert-danger")) mensagem.removeClass("alert-danger");
+                    if (!mensagem.hasClass("alert-success")) mensagem.addClass("alert-success");
+                    mensagem.removeClass("d-none");
+                    
+                    mensagem.html("<i class=\"material-icons\">&#xE002;</i> O registro foi salvo!" +
+                                  "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Fechar\"><span aria-hidden=\"true\">&times;</span></button>");
+
+                    $("#txtId").val(txtspl[1]);
+                }
+                else {
+                    var mensagem = $("#mensagem");
+
+                    if (mensagem.hasClass("alert-success")) mensagem.removeClass("alert-success");
+                    if (!mensagem.hasClass("alert-danger")) mensagem.addClass("alert-danger");
+                    mensagem.removeClass("d-none");
+
+                    mensagem.html("<i class=\"material-icons\">&#xE002;</i> " + text +
+                                  "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Fechar\"><span aria-hidden=\"true\">&times;</span></button>");
+                }
+            }
+        });
+    }
+
+    $("#cmdVoltar").click(function() {
+        location.href = "npcs.php";
+    });
+});
--- lib/npc.php
+++ lib/npc.php
@@ -54,5 +54,112 @@ class npc
 
         return $matriz;
     }
+
+    public function Selecionar($id)
+    {
+        $sql    = "SELECT npcId, npcNome, npcChave, n.eixoId, eixoNome, npcImgNormal, npcImgIcone, npcAtivo " .
+                  'FROM npc n ' .
+                  'LEFT JOIN eixos e ON e.eixoId = n.eixoId ' .
+                  "WHERE npcId = '$id' " .
+                  'ORDER BY npcNome';
+
+        $db     = new bancodados();
+        $res    = $db->SelecaoSimples($sql);
+
+        if ($res !== false)
+        {
+            if (count($res) > 0)
+            {
+                $npc   = $res[0];
+
+                $this->id                    = $npc[self::NPC_ID];
+                $this->nome                  = $npc[self::NPC_NOME];
+                $this->chave                 = $npc[self::NPC_CHAVE];
+                $this->eixo                  = $npc[self::EIXO_ID];
+                $this->eixoNome              = $npc[self::EIXO_NOME];
+                $this->imagemNormal          = $npc[self::NPC_IMGNORMAL];
+                $this->icone                 = $npc[self::NPC_IMGICONE];
+                $this->ativo                 = $npc[self::NPC_ATIVO];
+            }
+        }
+    }
+
+    public function Salvar()
+    {
+        if ($this->id == null)
+        {
+            $id     = '{ID}';
+        }
+        else
+        {
+            $id     = $this->id;
+        }
+
+        if ($this->eixo == '*')
+        {
+            $eixo   = 'NULL';
+        }
+        else
+        {
+            $eixo   = "'$this->eixo'";
+        }
+
+        if ($id == '{ID}')
+        {
+            return $this->Incluir($id, $eixo);
+        }
+        else
+        {
+            return $this->Atualizar($id, $eixo);
+        }
+    }
+
+    public function Incluir($id, $eixo)
+    {
+        $sql    = 'INSERT INTO npc ' .
+                  '(npcId, npcNome, npcChave, eixoId, npcImgNormal, npcImgIcone, npcAtivo) ' . 
+                  "VALUES ('$id', '$this->nome', '$this->chave', $eixo, $this->imagemNormal, $this->icone, $this->ativo)";
+
+        $db         = new bancodados();
+        $this->id   = $db->ExecutarRetornaId($sql);
+
+        if ($this->id != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function Atualizar($id, $eixo)
+    {
+        $sql    = 'UPDATE npc ' .
+                  "SET npcNome = '$this->nome', " .
+                  "npcChave = '$this->chave', " .
+                  "eixoId = $eixo, " .
+                  "npcImgNormal = $this->imagemNormal, " .
+                  "npcImgIcone = $this->icone, " .
+                  "npcAtivo = $this->ativo " .
+                  "WHERE npcId = '$id'";
+
+        $db         = new bancodados();
+        $db->Executar($sql);
+
+        return true;
+    }
+
+    public function Excluir()
+    {
+        $sql    = 'UPDATE npc ' .
+                  "SET npcAtivo = 0 " .
+                  "WHERE npcId = '$this->id'";
+
+        $db         = new bancodados();
+        $db->Executar($sql);
+
+        return true;
+    }    
 }
 ?>
